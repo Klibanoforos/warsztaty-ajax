139fece2eb397e73c3b19bd5abec4838b2ba766a
Bartosz Zdziech <klibanoforos@gmail.com>
2017-06-25 16:29:37 +0200

    o dziwo, działa

--- js/main.js
+++ js/main.js
@@ -1,2 +1,37 @@
 'use strict';
 
+
+//ogólne "body" funkcji jQuery
+
+$(function () {
+
+    
+//    stworzenie funkcji, która
+    
+    $(window).scroll(function () {
+        
+        
+//         uruchomi się po zeskrolowaniu na sam dół
+        
+        if (
+            $(window).scrollTop() + $(window).height() == $(document).height()) {
+
+            
+//            i ściągnie dane ajaxem jQuery
+            
+            $.getJSON('https://jsonplaceholder.typicode.com/users', function (data) {
+                console.log(data);
+
+                
+//                wyrzuci je na stronę
+                
+                data.forEach(function (element) {
+                        $('body').append('<p>Nazwa użytkownika: ' + element.name + '</p>');
+                        $('body').append('<p>ID użytkownika: ' + element.id + '</p>');
+                        $('body').append('<p>URL użytkownika: <a href="https://">' + element.website + '</a></p>' + '<hr>');
+
+                })
+            });
+        }
+    });
+});
